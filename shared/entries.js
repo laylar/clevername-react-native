2a697db968c73f524dac2b1b898f8dae5cb96a9f
Layla <layla.rowen@gmail.com>
2020-05-31 11:35:22 -0700

    #17 entries.js file

---
+++ shared/entries.js
@@ -0,0 +1,35 @@
+//I wonder if I use conditional formatting to display
+//the dates in different languages... 
+
+export const ENTRIES = [
+    {
+        id: 0,
+        date: '2020-5-30',
+        entry: 'This is some example text...'
+    },
+    {
+        id: 1,
+        date: '2020-5-29',
+        entry: 'This is some example text...'
+    },
+    {
+        id: 2,
+        date: '2020-5-28',
+        entry: 'This is some example text...'
+    },
+    {
+        id: 3,
+        date: '2019-5-30',
+        entry: 'This is some example text...'
+    },
+    {
+        id: 4,
+        date: '2019-5-29',
+        entry: 'This is some example text...'
+    },
+    {
+        id: 5,
+        date: '2018-5-28',
+        entry: 'This is some example text...'
+    },
+]
