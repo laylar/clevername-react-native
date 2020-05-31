86388bcefde2f935a02a1fdd676436e7e33c5201
Layla <layla.rowen@gmail.com>
2020-05-31 11:47:13 -0700

    #5 set aside placeholder for date in EntryComponent

--- components/EntryComponent.js
+++ components/EntryComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { View, Text } from 'react-native';
 
 function Entry(props) {
 
@@ -14,11 +15,14 @@ function Entry(props) {
     };
 
     return (
-        <FlatList
-            data={props.entries}
-            renderItem={renderEntryItem}
-            keyExtractor={item => item.id.toString()}
-        />
+        <View>
+            <Text>Today's Date is:</Text>
+            <FlatList
+                data={props.entries}
+                renderItem={renderEntryItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </View>
     );
 }
 
