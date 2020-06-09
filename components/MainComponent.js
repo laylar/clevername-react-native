60c07d227c591fb78df6bed1868c7d421236295a
Layla <layla.rowen@gmail.com>
2020-06-09 14:23:51 -0700

    drawer icon

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, View, Platform, SafeAreaView, ScrollView, Image } from 'react-native';
 import { ENTRIES } from '../shared/entries'
 import EntrySnippet from './EntrySnippetComponent';
 import EntryInfo from './EntryInfoComponent';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation';
 import { Icon } from 'react-native-elements';
+
+
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -59,16 +61,55 @@ const HomeNavigator = createStackNavigator(
     }
 )
 
+const CustomDrawerContentComponent = props => (
+    <ScrollView>
+        <SafeAreaView
+            style={styles.container}
+            forceInset={{ top: 'always', horizontal: 'never' }}>
+            <View style={{ flex: 1 }}>
+                <Image source={require('./images/medlogoSouviengraydark.png')} style={styles.drawerImage} />
+                <View style={{ flex: 2 }}>
+                    <Text style={styles.drawerHeaderText}>Remember</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} />
+        </SafeAreaView>
+    </ScrollView>
+);
+
 const MainNavigator = createDrawerNavigator(
     {
-        Home: { screen: HomeNavigator },
-        Entries: { screen: DirectoryNavigator }
+        Home: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                drawerIcon: () => (
+                    <Icon
+                        name='home'
+                        type='font-awesome'
+                        size={24}
+                    />
+                ),
+
+            }
+        },
+        Entries: {
+            screen: DirectoryNavigator,
+            navigationOptions: {
+                drawerIcon: () => (
+                    <Icon
+                        name='pencil-square-o'
+                        type='font-awesome'
+                        size={24}
+
+                    />
+                )
+            }
+        }
     },
     {
-        drawerBackgroundColor: 'gray'
+        drawerBackgroundColor: 'gray',
+        contentComponent: CustomDrawerContentComponent
     }
-
-
 );
 
 class Main extends Component {
@@ -82,6 +123,26 @@ class Main extends Component {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#eaeaea',
+        fontSize: 24,
+    },
+    drawerImage: {
+        margin: 1,
+        height: 88,
+        width: 297
+    },
     stackIcon: {
         marginLeft: 10,
         color: '#fff',
